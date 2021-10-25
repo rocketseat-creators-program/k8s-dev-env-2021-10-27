a7489dad4dacd53ea9bead5d8e7de5b29ccd5155
Alexandre Paez Sequeira <alepaezseq@gmail.com>
2021-10-25 01:02:27 -0300

    shutting down local registry

--- scripts/cluster_down.sh
+++ scripts/cluster_down.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 kind delete cluster --name k8s-dev-env
+
+echo 'Shutting down local registry ...'
+docker kill k8s-dev-env-registry
+docker rm k8s-dev-env-registry
+
+echo 'Cluster down'
+
