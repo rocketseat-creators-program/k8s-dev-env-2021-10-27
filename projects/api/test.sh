76a8608400366e0e51671d9599f2f70857d7df35
Alexandre Paez Sequeira <alepaezseq@gmail.com>
2021-10-26 13:11:10 -0300

    Example API up

--- projects/api/test.sh
+++ projects/api/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -XPOST http://localhost:3000/users/create -H 'Content-Type: application/json' -d '{ "username": "tester" }'
+curl -XPOST http://localhost/users/create -H 'Content-Type: application/json' -d '{ "username": "tester" }'
 
-curl -XGET http://localhost:3000/users
+curl -XGET http://localhost/users
 
